f8061e122c4ab54287a9864af9dd7c56610c6d0a
Polina_Berezhneva <stiripo13@gmail.com>
2024-09-23 10:02:07 -0400

    Create helper function to extract flight data

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import './App.css';
 import { FlightInfo } from './components/FlightInfo';
 import { myFlight } from './sample_flight-info';
+import { extractFlightData } from './utils';
+import { downloadedFlight } from './sample_flight-info';
+
+let extracted = extractFlightData(downloadedFlight);
+console.log(extracted.carrier);
+console.log(extracted.price);
+console.log(extracted.departureCity);
+console.log(extracted.arrivalCity);
+console.log(extracted.departureAirport.caption);
+console.log(extracted.departureAirport.uid);
+
 
 function App() {
 
---
+++ src/utils.ts
@@ -0,0 +1,48 @@
+export interface ExtractedFlightData {
+    'carrier': string,
+    'price': number,
+    'departureCity': string,
+    'departureAirport': {
+        'caption': string,
+        'uid': string,
+    },
+    'arrivalCity': string,
+    'arrivalAirport': {
+        'caption': string,
+        'uid': string,
+    },
+}
+
+export function extractFlightData(data: any): ExtractedFlightData {
+    let result: ExtractedFlightData = {
+        carrier: '',
+        price: 0,
+        departureCity: '',
+        departureAirport: {
+            caption: '',
+            uid: '',
+        },
+        arrivalCity: '',
+        arrivalAirport: {
+            caption: '',
+            uid: ''
+        }
+    };
+
+    result.carrier = data.flight.carrier.caption;
+    result.price =  data.flight.price.total.amount;
+
+    let outboundSegments =  data.flight.legs[0].segments;
+    result.departureCity = outboundSegments[0].departureCity.caption;
+    result.departureAirport = {
+        'caption': outboundSegments[0].departureAirport.caption,
+        'uid': outboundSegments[0].departureAirport.uid,
+    };
+
+    result.arrivalCity = outboundSegments[outboundSegments.length - 1].arrivalCity.caption;
+    result.arrivalAirport = {
+       'caption': outboundSegments[0].arrivalAirport.caption,
+        'uid': outboundSegments[0].arrivalAirport.uid,
+    };
+    return result;
+}
